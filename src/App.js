f392a8c80baa808bae012abe19b8965c045ddaf3
Jose Mejia <jose.mejia04@gmail.com>
2023-02-08 10:37:42 -0500

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Display from "./components/Display";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div className="App">
+      <Display />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
